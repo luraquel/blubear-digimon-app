dcd3a3b36e2b71f55a084db6279a963c453d056d
Lupe Herrera <luraquel@hotmail.com>
2024-04-01 10:45:46 -0500

    feat: add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    projectId: 'blubear-pokemon',
+    appId: '1:794339954662:web:586d671373766fe0635027',
+    storageBucket: 'blubear-pokemon.appspot.com',
+    apiKey: 'AIzaSyA6rl461y0uyvoyZRTduzVJQbarCej7p0Q',
+    authDomain: 'blubear-pokemon.firebaseapp.com',
+    messagingSenderId: '794339954662',
+  },
+  digimonUrl: 'https://digi-api.com/api/v1',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'blubear-pokemon',
+    appId: '1:794339954662:web:586d671373766fe0635027',
+    storageBucket: 'blubear-pokemon.appspot.com',
+    apiKey: 'AIzaSyA6rl461y0uyvoyZRTduzVJQbarCej7p0Q',
+    authDomain: 'blubear-pokemon.firebaseapp.com',
+    messagingSenderId: '794339954662',
+  },
+  digimonUrl: 'https://digi-api.com/api/v1',
 };
 
 /*
